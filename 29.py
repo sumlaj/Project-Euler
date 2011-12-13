47b26b2a98445242c5c268ef8d13c001750b4fb3
Sumlaj <sumlaj@Nirappil.(none)>
2011-12-13 10:05:40 +0530

    files renamed(PEP8) and reduced to 10

--- 29.py
+++
@@ -1,8 +0,0 @@
-import time
-tStart = time.time()
-s=set()
-for a in range(2,101):
-    for b in range(2,101):
-        s.add(a**b)
-print len(s)
-print "Run Time = " + str(time.time() - tStart)
--- 52.py
+++
@@ -1,6 +0,0 @@
-for i in range(1,1000000):
-    if set(str(i)) == set(str(2*i)) == set(str(3*i)) == set(str(4*i)) == set(str(5*i)) == set(str(6*i)):
-        print i
-        break
-    else:
-        continue
