13d3c6ca3df17120e78dfa5e2aa32754f80a4c73
Sumlaj <sumlaj@Nirappil.(none)>
2011-12-13 12:11:34 +0530

    PEP8 compliant,added comments

--- PU30.py
+++ PU30.py
@@ -1,13 +1,29 @@
+'''PE 30
+http://projecteuler.net/problem=30
+Surprisingly there are only three numbers that can be written
+ as the sum of fourth powers of their digits:
+
+1634 = 14 + 64 + 34 + 44
+8208 = 84 + 24 + 04 + 84
+9474 = 94 + 44 + 74 + 44
+As 1 = 14 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as 
+the sum of fifth powers of their digits.
+'''
+
 import time
 tStart = time.time()
 
-s=0
-for i in range(2,200000):
-    d=0
+s = 0
+for i in xrange(2, 200000):#assumed a maximum range
+    d = 0
     for n in str(i):
-        d=d+int(n)**5
-    if d==i:
-        s=s+i
+        d = d+int(n)**5#finding the sum of fifth powers
+    if d == i:
+        s = s+i
 print s
 
 print "Run Time = " + str(time.time() - tStart)
