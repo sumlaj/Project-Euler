03fdc3bada52b673bd51c6b9bfa7aa3a4e7fd740
Sumlaj <sumlaj@Nirappil.(none)>
2011-12-13 11:39:44 +0530

    PEP8 compliant,added comments

--- PU24.py
+++
@@ -1,5 +0,0 @@
-import time
-tStart = time.time()
-import itertools
-print list(itertools.permutations(range(10)))[999999]
-print "Run Time = " + str(time.time() - tStart)
--- PU25.py
+++ PU25.py
@@ -1,12 +1,33 @@
+''' PE 25
+http://projecteuler.net/problem=25
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn1 + Fn2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the first term in the Fibonacci sequence to contain 1000 digits?
+'''
 import time
 tStart = time.time()
-a,b,t=1,2,0
-for i in range(10000):
-    if len(str(b)) == 1000:
-        print b,i+3
+a, b = 1, 2
+for i in xrange(10000):
+    if len(str(b)) == 1000: #checking whether it's 1000 digit
+        print i+3
         break
     else:
-        t=b
-        b=b+a
-        a=t
+        a, b = b, a+b #swapping value of b to a and a+b to b
 print "Run Time = " + str(time.time() - tStart)
